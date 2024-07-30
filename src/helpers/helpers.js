bf82ffe7150e4856aeb5f5710398cfe5d6adfe5f
Boji <prxboji@gmail.com>
2024-07-29 17:50:35 -0700

    Edit : Amount

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,11 +3,10 @@ const randomAmount = () => {
     try {
 
         const randomNumber = Math.random();
-        const randomValue = Math.floor(randomNumber * (15 - 5 + 1)) + 5;
-        return randomValue;        
+        const randomValue = Math.floor(randomNumber * (150 - 80 + 1)) + 80;
+        return randomValue;     
         
     } catch (e) {
-        console.error('Error randomize amount:', error);
         return 'Amount Errors';
     }
 
