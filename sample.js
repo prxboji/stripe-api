9acb0055f92d34d219b4e15df4d40b4ba7e30e8e
Boji <prxboji@gmail.com>
2024-08-14 12:48:50 -0700

    Add : Sample

--- sample.js
+++ sample.js
@@ -3,7 +3,12 @@ const bot = new STRIPE();
 
 (async () => {
 
-    const createPaymentMethod = await bot.createPaymentMethod('4242424242424242|09|28|550');
-    console.log(createPaymentMethod);
+    const createPaymentMethod = await bot.createPaymentMethod('4242424242424241', '09', '28', '550');
+    if (createPaymentMethod.id) {
+        console.log(createPaymentMethod);
+        
+    } else {
+        console.log(`[DIED] ${createPaymentMethod.error.message}`);
+    }
 
 })();
